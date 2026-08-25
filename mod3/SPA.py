aebfef651bd8bf714e6359ecb19d48eca70fd999
GokhanKorkmaz <gokhan.korkmaz@metropolia.fi>
2026-08-25 13:09:08 +0300

    Sum,Average, Product

---
+++ mod3/SPA.py
@@ -0,0 +1,11 @@
+number1 = int(input("Enter the first integer "))
+number2 = int(input("Enter the second integer "))
+number3 = int(input("Enter the third integer "))
+
+sum = number1 + number2 + number3
+product = number1 * number2 * number3 
+average = sum / 3
+
+print("Sum: ",sum)
+print("Product: ",product)
+print("Average: ",average);
