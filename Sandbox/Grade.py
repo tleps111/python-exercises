df735d497cf127760b0103d71d2c976241f91455
GokhanKorkmaz <gokhan.korkmaz@metropolia.fi>
2026-08-25 15:18:02 +0300

    25.8 if elif exercieses classroom

--- Sandbox/Grade.py
+++ Sandbox/Grade.py
@@ -1,19 +1,3 @@
-"""score = int(input("Enter your score: "))
-if 90 <= score <= 100:
-    print("Your grade is A1") 
-elif 80 <= score < 90:
-     print("Your grade is A2") 
-elif 70 <= score < 80:
-    print("Your grade is B1") 
-elif 70 <= score < 60:   
-     print("Your grade is B2") 
-elif 60 <= score <= 50:  
-   print("Your grade is C") 
-else: 
-    print = ("Failed")
-"""
-
-
 score = int(input("Enter your score: "))
 if score >= 90:
     print("Your grade is A1") 
