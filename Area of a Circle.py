cd645b3126bd9252e60294f0be37f02522a8dcf0
GokhanKorkmaz <gokhan.korkmaz@metropolia.fi>
2026-08-25 08:54:12 +0300

    Import maths has been applied to Area of circle

---
+++ Area of a Circle.py
@@ -0,0 +1,4 @@
+import math
+radius = float(input("Enter the radius in cm: "))
+area = math.pi * radius * radius
+print(f"The area of the circle is: {area:.2f} cm2")
--- mod01-02/Area of a Circle.py
+++
@@ -1,3 +0,0 @@
-radius = float(input("Enter the radius in cm: "))
-area = 3.14 * radius * radius
-print("The area of the circle is: " + str(area) + " cm2")
