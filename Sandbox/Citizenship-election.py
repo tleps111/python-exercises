510f629907b5b93ccc67f0c8bbd0a49dd762139c
GokhanKorkmaz <gokhan.korkmaz@metropolia.fi>
2026-08-26 14:00:09 +0300

    sandbox commit

--- Sandbox/Citizenship-election.py
+++ Sandbox/Citizenship-election.py
@@ -1,6 +1,6 @@
 ###if the person is citizen and the if yes then ask the age if over 18+ "you are eligible for voting"###
 citizenship = input("Are you a citizen? ")
-if citizenship == "yes":
+if citizenship.lower() == "yes":
     age = int(input("Enter your age: "))
     if age >= 18:
         print("You can vote")
