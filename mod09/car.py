14f602154b3182cfeff7782552243ec15d1ddf4f
GokhanKorkmaz <gokhan.korkmaz@metropolia.fi>
2026-09-16 21:32:18 +0300

    car

--- mod09/car.py
+++ mod09/car.py
@@ -1,12 +1,12 @@
 class Car:
-    def __init__(self, registeration_number, max_speed, current_speed, travelled_distance):
-        self.registeration_number = registeration_number
+    def __init__(self, registration_number, max_speed):
+        self.registration_number = registration_number
         self.max_speed = max_speed
-        self.current_speed = current_speed
-        self.travelled_distance = travelled_distance
+        self.current_speed = 0
+        self.travelled_distance = 0
 
-car1 = (Car("Toyota", "Red", 1984))
-car2 = (Car("Volkswagen", "Blue", 1980))
-car3 = (Car("Volvo", "Brown", 2005))
-print(f"Car1's name: {car1.brand}, Color: {car1.color}, Production year: {car1.year} ")
-print(f"Car2's name: {car2.brand}, Color: {car2.color}, Production year: {car2.year} ")
+car1 = (Car("ABC-123", 142))
+print(f"Registration number: {car1.registration_number}, Max Speed: {car1.max_speed} km/h, Current Speed: {car1.current_speed} km/h, Travelled Distance: {car1.travelled_distance} km")
+        
+        
+
